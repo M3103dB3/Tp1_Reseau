fa110ed5429cb81442a000462fd7f9451fca85ae
M3103dB3 <mehdi130897@gmail.com>
2017-03-24 10:18:05 +0100

    Add files via upload

--- client2.py
+++ client2.py
@@ -3,8 +3,7 @@ serverName = "localhost"
 serverPort = 13000
 clientSocket2 = socket(AF_INET, SOCK_STREAM)
 clientSocket2.connect((serverName,serverPort))
-sentence = raw_input('Input lowercase sentence:')
-clientSocket2.send(sentence)
+
 modifiedSentence = clientSocket2.recv(1024)
 print 'From Server:', modifiedSentence
 clientSocket2.close()    
--- filtre.py
+++ filtre.py
@@ -1,31 +1,26 @@
 from socket import *
+serverName ="localhost"
 serverPort1= 12000
+serverPort2 = 13000
+
 serverSocket1 = socket(AF_INET,SOCK_STREAM)
 serverSocket1.bind(('',serverPort1))
 serverSocket1.listen(1)
 
-serverPort2 = 13000
 serverSocket2 = socket(AF_INET,SOCK_STREAM)
 serverSocket2.bind(('',serverPort2))
 serverSocket2.listen(1)
-
-
-
 print 'The server is ready to receive'
-while 1:
-	connectionSocket1, addr = serverSocket1.accept()
-	connectionSocket2, addr = serverSocket2.accept()
-	
-	sentence1 = connectionSocket1.recv(1024)
-	capitalizedSentence1 = sentence1.upper()
-	connectionSocket2.send(capitalizedSentence1)
-	print capitalizedSentence1	
-	connectionSocket1.close()
 
-	sentence2 = connectionSocket2.recv(1024)
-	capitalizedSentence2 = sentence2.upper()
-	connectionSocket1.send(capitalizedSentence2)
-	print capitalizedSentence	
+while 1:
+	connectionSocket, addr = serverSocket1.accept()
+	sentence = connectionSocket.recv(1024)
+	modifiedSentence = sentence.upper()
+	connectionSocket.send(modifiedSentence)
+	connectionSocket.close()
+
+	connectionSocket2, addr = serverSocket2.accept()
+	connectionSocket2.send(modifiedSentence)
 	connectionSocket2.close()
 
 
